1e8dd02b74958d52237dded0c569daa1ea058d0c
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2022-02-05 19:51:07 -0300

    Criando o component de extrato

---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia.service';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.scss']
+})
+export class ExtratoComponent implements OnInit {
+  transferencias: any[];
+
+  constructor(private transferenciaService: TransferenciaService) { }
+
+  ngOnInit(): void {
+    this.transferencias = this.transferenciaService.transferencias;
+  }
+
+}
